0986be7e4d712a2f5c2c1475f91e09c2b5e4825a
k4rd4k <abhishek.kardak1@gmail.com>
2019-08-11 02:49:29 -0500

    added prompts to form fields

--- forms.py
+++ forms.py
@@ -3,37 +3,37 @@ from wtforms import StringField, BooleanField, PasswordField, SubmitField, Integ
 from wtforms.validators import *
 
 class LoginForm(FlaskForm):
-    username = StringField('User Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('User Name', validators=[DataRequired()],render_kw={"placeholder": "Enter your username"})
+    password = PasswordField('Password', validators=[DataRequired()],render_kw={"placeholder": "Enter your password"})
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
 
 
 class SignUpForm(FlaskForm):
-	firstname = StringField('First Name')
-	lastname = StringField('Last Name')
-	zipcode = IntegerField('Zip Code')
-	gender = StringField('Gender')
-	username = StringField('UserName', validators=[DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+	firstname = StringField('First Name',render_kw={"placeholder": "Enter first name"})
+	lastname = StringField('Last Name',render_kw={"placeholder": "Enter last name"})
+	zipcode = IntegerField('Zip Code',render_kw={"placeholder": "Enter zip code"})
+	gender = StringField('Gender',render_kw={"placeholder": "Enter gender"})
+	username = StringField('UserName', validators=[DataRequired(), Length(min=2, max=20)],render_kw={"placeholder": "Select a username"})
+	email = StringField('Email', validators=[DataRequired()],render_kw={"placeholder": "Enter email ID"})
+	password = PasswordField('Password', validators=[DataRequired()],render_kw={"placeholder": "Select a strong password"})
+	confirm_password = PasswordField('Confirm Password', validators=[DataRequired()],render_kw={"placeholder": "Re-enter the password"})
 	submit = SubmitField('Sign Up')
 
 class ZipSearchForm(FlaskForm):
-	zipcode = IntegerField('Zip Code', validators=[DataRequired()])
+	zipcode = IntegerField('Zip Code', validators=[DataRequired()],render_kw={"placeholder": "Enter the zip code to search"})
 	submit = SubmitField('Search this area')
 
 class SlotDateSearchForm(FlaskForm):
-	starttime = IntegerField('Start Time(24-hr clock)', validators=[DataRequired()])
-	zipcode = IntegerField('End Time(24-hr clock)', validators=[DataRequired()])
-	eventdate = StringField('Date', validators=[DataRequired()])
+	starttime = IntegerField('Start Time(24-hr clock)', validators=[DataRequired()],render_kw={"placeholder": "Enter start time hour"})
+	zipcode = IntegerField('End Time(24-hr clock)', validators=[DataRequired()],render_kw={"placeholder": "Enter end time hour"})
+	eventdate = StringField('Date', validators=[DataRequired()],render_kw={"placeholder": ""})
 	submit = SubmitField('Search Events')
 
 class VenueDateForm(FlaskForm):
-	zipcode = IntegerField('Zip Code', validators=[DataRequired()])
-	eventdate = StringField('Date', validators=[DataRequired()])
+	zipcode = IntegerField('Zip Code', validators=[DataRequired()],render_kw={"placeholder": "Enter zipcode"})
+	eventdate = StringField('Date', validators=[DataRequired()],render_kw={"placeholder": "Enter date"})
 	submit = SubmitField('Search Events')
 
 # class StartEventForm():
